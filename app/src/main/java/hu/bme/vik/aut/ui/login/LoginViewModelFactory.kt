3be8e09418ad676a1e39a6b69fff1e0b6e22e954
Peter Haulik <xhaulik@stuba.sk>
2022-11-09 15:02:31 +0100

    init commit

---
+++ app/src/main/java/hu/bme/vik/aut/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package hu.bme.vik.aut.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import hu.bme.vik.aut.data.LoginDataSource
+import hu.bme.vik.aut.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
